ff7306ecb4019e7d0efe94ff4e077d9b050fec8f
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-30 17:15:44 -0500

    took of incerteptors

--- src/index.js
+++ src/index.js
@@ -5,23 +5,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
-axios.interceptors.request.use(request => {
-    console.log(request);
-    //Edit request config;
-    return request;
-}, error => {
-    console.log(error);
-    Promise.reject(error);
-});
-
-axios.interceptors.response.use(response => {
-    console.log(response);
-    //Edit request config;
-    return response;
-}, error => {
-    console.log(error);
-    Promise.reject(error);
-});
-
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
