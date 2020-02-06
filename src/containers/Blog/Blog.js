e05b77a4b1be89dffd5321541a6db84120fda357
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-02-05 19:49:51 -0500

    turning our components to a containers and adding Posts.js prepariang project for Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,54 +1,11 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
-import axios from '../../Axios';
-
-import Post from '../../components/Post/Post';
+import Posts from './Posts/Posts';
 
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-    
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatePosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'OLEG'
-                    }
-                });
-                this.setState({posts: updatePosts});
-                //console.log(response);
-            })
-                .catch(error => {
-                    //console.log(error);
-                    this.setState({error: true});
-                });
-    }
-
-    postSelectedHandler (id) {
-        this.setState({selectedPostId: id});
-    }
-
+   
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                            key={post.id}
-                            title={post.title}
-                            author={post.author}
-                            clicked= {() => this.postSelectedHandler(post.id)} />;
-            });
-        }
-        
-
         return (
             <div className="Blog">
                 <header>
@@ -59,10 +16,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-              
+            <Posts />
             </div>
         );
     }
